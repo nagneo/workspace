915ab7846bcca67b2c44a25e2d5febbdf9401fff
SaenaKIM <tosk3927@gmail.com>
2022-02-08 20:54:28 +0900

    Change Korean to English (Chapter03_01)

--- python/Introduction/chapter03_01.py
+++ python/Introduction/chapter03_01.py
@@ -1,20 +1,20 @@
 # Chapter03-1
-# 숫자형
+# number type variable
 
-# 파이썬 지원 자료형
+# Data type supported by python 
 '''
-int : 정수
-float : 실수
-complex : 복소수
-bool : 불린
-str : 문자열(시퀀스)
-list : 리스트(시퀀스)
-tuple : 튜플(시퀀스)
-set : 집합
-dict : 사전
+int : integer
+float : real number
+complex : complex
+bool : boolean
+str : string(sequence)
+list : list(sequence)
+tuple : tuple(sequence)
+set : set
+dict : dictionary(map)
 '''
 
-# 데이터 타입
+# Data type
 str1 = "Python"
 bool = True
 str2 = "Anaconda"
@@ -28,7 +28,7 @@ dict = {
 tuple = 3, 5, 7
 set = {7, 8, 9}
 
-# 데이터 타입 출력
+# Print data type
 print(type(str1))
 print(type(bool))
 print(type(str2))
@@ -39,46 +39,46 @@ print(type(dict))
 print(type(tuple))
 print(type(set))
 
-# 숫자형 연산자
+# number operator
 """
 + 
 - 
 * 
 / 
-// : 몫 
-% : 나머지
+// : quotient
+% : remainder
 abs(x) 
 int(x) 
 float(x) 
 complex(x)
 pow(x, y) 
-x ** y : 제곱
+x ** y : square
 ....
 """
 
-# 정수 선언
+# Declare integer
 i = 77
 i2 = -14
 big_int = 999999999999999999999999999999999999999
 
-# 정수 출력
+# Print integer
 print(i)
 print(i2)
 print(big_int)
 
-# 실수 선언
+# Declare float
 f = 0.9999
 f2 = 3.141592
 f3 = -3.9
 f4 = 3 / 9
 
-# 실수 출력
+# Print float
 print(f)
 print(f2)
 print(f3)
 print(f4)
 
-# 연산 실습
+# Exercise
 i1 = 39
 i2 = 939
 big_int1 = 123456789123456789012345678901234567890
@@ -128,38 +128,42 @@ print("2 ** 3: ", 2 ** 3)
 print("i1 ** i2: ", i1 ** i2) 
 print("f1 ** f2: ", f1 ** f2)
 
-# 형 변환 실습
+# Exercise of type casting
 a = 3.
 b = 6
 c = .7
 d = 12.7
 
-# 타입 출력
+# print type
 print(type(a), type(b), type(c), type(d))
 
-# 형 변환
-print(float(b))  # 정수 -> 실수
-print(int(c))  # 실수 -> 정수
-print(int(d))  # 실수 -> 정수
-print(int(True))  # Bool -> 정수
-print(float(True))  # Bool -> 정수
-print(int(False))  # Bool -> 정수
-print(float(False))  # Bool -> 정수
-print(complex(3))  # 정수 -> 복소수
-print(complex('3'))  # 문자 -> 복소수
-print(complex(False))  # Bool -> 복소수
+# type casting
+del float
+del int
+del bool
 
-# 수치 연산 함수
+print(float(b))  # integer -> float
+print(int(c))  # float -> integer
+print(int(d))  # float -> integer
+print(int(True))  # Bool -> integer
+print(float(True))  # Bool -> float
+print(int(False))  # Bool -> integer
+print(float(False))  # Bool -> float
+print(complex(3))  # integer -> complex
+print(complex('3'))  # character -> complex
+print(complex(False))  # Bool -> complex
+
+# Numerical calculation function
 print(abs(-7))
 x, y = divmod(100, 8)
-print(x, y)
-print(pow(5, 3))
+print("divmod(100,8): {} {}".format(x, y))
+print("pow(5,3): ".format(pow(5, 3)))
 
-#외부 모듈
+#import external module
 import math
 
 #ceil
-print(math.ceil(5.1))   # x 이상의 수 중에서 가장 작은 정수
+print(math.ceil(5.1))   # The smallest integer among the numbers above x
 
 #floor
 
